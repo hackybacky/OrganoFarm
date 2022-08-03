7816fb62f79cecacf6a525ba20975419e308a054
hackybacky <hacky3244@gmail.com>
2022-08-03 19:09:28 +0530

    d

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -85,12 +85,12 @@ const Menu = ({darkMode , setDarkMode}) => {
   return (
     <Container>
       <Wrapper>
-        {/* <Link to="/" style={{ textDecoration: "none", color: "inherit" }}> */}
-        <Logo>
-          <Img src={Clogo} />
-          OrganoFarm
-        </Logo>
-        {/* </Link> */}
+        <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
+          <Logo>
+            <Img src={Clogo} />
+            OrganoFarm
+          </Logo>
+        </Link>
         <Item>
           <HomeIcon />
           Home
--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import styled from 'styled-components'
 export default function Video() {
   return (
     <div>Video</div>
