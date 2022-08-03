8dd0a814a03573106ce34cf0bf2dadbb6bcc2ed2
hackybacky <hacky3244@gmail.com>
2022-08-03 19:35:01 +0530

    d

--- src/pages/Video.jsx
+++ src/pages/Video.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
 import styled from 'styled-components'
+const Container = styled.div`
+  gap:24px;
+  display :flex;
+`;
+
+const Content = styled.div`
+
+  flex :5 ;
+`;
+
+const Recommendation= styled.div`
+  flex :2 
+`;
+const VideoWrapper= styled.div`
+`;
+const Title = styled.h1``;
+const Details= styled.div``;
+const Info = styled.span``;
+const Buttons = styled.div``;
+const Button = styled.div``;
 export default function Video() {
   return (
-    <div>Video</div>
+    <Container>
+      <Content>
+
+        <VideoWrapper>
+          <iframe
+              width="100%"
+              height="720"
+              src="https://www.youtube.com/embed/k3Vfj-e1Ma4"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+        </VideoWrapper>
+      </Content>
+      <Recommendation>recommendation</Recommendation>
+    </Container>
   )
 }
