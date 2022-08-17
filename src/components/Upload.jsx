8c7516059cc77b571e749f0e23a55a172b5f8be9
hackybacky <hacky3244@gmail.com>
2022-08-17 19:27:28 +0530

    video upload page all hooks done

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -67,18 +67,26 @@ const Label = styled.label`
 const Upload = ({setOpen}) => {
   const [img, setImg]=useState(undefined);
   const [video , setVideo]=useState(undefined);
+  const [imgPerc,setImgPerc]=useState(0);
+  const [videoPerc , setVideoPerc]=useState(0);
+  const [title, setTitle ]=useState("");
+  const [desc , setDesc]=useState("");
+  const [tags,setTags]=useState([]);
+  const handleTags=(e)=>{
+    setTags(e.target.value.split(','))
+  }
   return (
     <Container>
       <Wrapper>
           <Close onClick={()=>{setOpen(false);console.log("hello")}}>X</Close>
           <Title>Upload a video</Title>
           <Label>Video : </Label>
-          <Input type ="file" accept="video/*"/>
-          <Input type ="text" placeholder='title'/>
-          <Desc placeholder='Description' row={8}/>
-          <Input type ="text" placeholder='seperate the tags with commas'/>
+          <Input type ="file" accept="video/*" onChange={(e)=>setVideo(e.target.files[0])}/>
+          <Input type ="text" placeholder='title' onChange={e=>setTitle(e.target.value)}/>
+          <Desc placeholder='Description' row={8} onChange={e=>setDesc(e.target.value)}/>
+          <Input type ="text" placeholder='seperate the tags with commas' onClick={handleTags}/>
           <Label>Image</Label>
-          <Input type ="file" accept="image/*"/>
+          <Input type ="file" accept="image/*" onChange={(e)=>setImg(e.target.files[0])}/>
           <Button>Upload</Button>
       </Wrapper>
     </Container>
