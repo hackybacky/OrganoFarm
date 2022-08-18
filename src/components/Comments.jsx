6f69b38d0fe7d7d3970b9386633976f32174f4a4
hackybacky <hacky3244@gmail.com>
2022-08-19 01:58:17 +0530

    comment section added

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -4,12 +4,31 @@ import Comment from "./Comment";
 import { useState,useEffect } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
-const Container = styled.div``;
+const Container = styled.div`
+
+  
+`;
 
 const NewComment = styled.div`
   display: flex;
   align-items: center;
+  // width:300px;
+  // height:300px;
   gap: 10px;
+`;
+const Hr = styled.hr`
+  margin: 15px 0px;
+  border: 0.5px solid ${({ theme }) => theme.soft};
+`;
+const Button = styled.button`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  width:200px;
+  height:40px;
+  cursor: pointer;
+  background-color:${({theme})=>theme.text};
+  color:${({theme})=>theme.bgLighter}
 `;
 
 const Avatar = styled.img`
@@ -31,6 +50,7 @@ const Input = styled.input`
 const Comments = ({videoId}) => {
   const currentUser = useSelector((state)=>state.user);
   const [comments,setComments]=useState([]);
+  const [newComment , setNewComment]=useState();
   useEffect(()=>{
     const fetchComments =async()=>{
      try{
@@ -42,12 +62,20 @@ const Comments = ({videoId}) => {
     }
     fetchComments();
   },[videoId])
+  const addComment= async()=>{
+    console.log("hello")
+    const res = await axios.post("/comments",{desc:newComment,videoId});
+    console.log(res);
+  }
   return (
-    <Container>
-      <NewComment>
+    <Container >
+      <NewComment   >
         <Avatar src={currentUser.img} />
-        <Input placeholder="Add a comment..." />
+        <Input placeholder="Add a comment..." onChange={(e)=>{setNewComment(e.target.value);console.log(newComment);}} />
+        <Button onClick={addComment} ><h2>Add Comment</h2></Button>
+        
       </NewComment>
+      <Hr/>
       {
         comments.map((comment)=>(
           <Comment comment = {comment} key ={comment._id}/>
